c8a4bea73ca009a0b03066e5d3c5e78c2cad80a9
Chigozie <ogilichigozie32@gmail.com>
2025-08-22 17:12:48 -0700

    Updated code

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const heroSlides = [
     title: "What's playing?",
     desc: 'Save, add to watchlist and watch<br>with your favourite available<br>application.',
     cta: 'Add to Watchlist',
-    bg: 'images/Karen-zhao.jpg',
+    bg: 'images/karen-zhao.jpg',
     img: 'images/Spider man.jpg'
   }
 ];
